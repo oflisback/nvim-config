d14fb6b50a0cdb7767585b8b3c4b26863353f888
Ola Flisbäck <ola.flisback@gmail.com>
2024-06-15 15:18:46 +0200

    Add git-fugitive

---
+++ lua/user/plugins/git-fugitive.lua
@@ -0,0 +1,13 @@
+return {
+	"tpope/vim-fugitive",
+	config = function()
+		local wk = require("which-key")
+
+		wk.register({
+			name = "Git",
+			a = { "<cmd>Git commit --amend<CR>", "Amend commit" },
+			c = { "<cmd>Git commit --verbose<CR>", "Git commit" },
+		}, { prefix = "<leader>g" })
+	end,
+	event = "VeryLazy",
+}
