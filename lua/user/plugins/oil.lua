d4252ebab65a37dfe51a78075a6816cbb346313e
Ola Flisbäck <oflisbaeck@apple.com>
2024-06-19 16:51:11 +0200

    Oil config tweak

--- lua/user/plugins/oil.lua
+++ lua/user/plugins/oil.lua
@@ -2,6 +2,15 @@ return {
 	"stevearc/oil.nvim",
 	lazy = false,
 	opts = {},
+
+	config = function()
+		require("oil").setup({
+			columns = { "icon" },
+			view_options = {
+				show_hidden = true,
+			},
+		})
+	end,
 	-- Optional dependencies
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
