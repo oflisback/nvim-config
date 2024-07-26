a729612a9583af46c5b128cca5d3da9f3a4f47b2
Ola Flisbäck <ola.flisback@gmail.com>
2024-07-26 23:01:38 +0200

    Disable which-key preset plugin
    It messes with vim-fugitive c commands in :Git window.

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -1,6 +1,21 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	config = function()
+		require("which-key").setup({
+			plugins = {
+				presets = {
+					operators = false, -- disables the builtin operator-pending mappings
+					motions = false, -- disables the builtin motion mappings
+					text_objects = false, -- disables the builtin text-object mappings
+					windows = false, -- disables the builtin windows mappings
+					nav = false, -- disables the builtin nav mappings
+					z = false, -- disables the builtin z mappings
+					g = false, -- disables the builtin g mappings
+				},
+			},
+		})
+	end,
 	init = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 300
