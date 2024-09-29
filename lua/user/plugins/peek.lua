74893b236bd11e499f070fec1695fe9be40c8512
Ola Flisbäck <oflisbaeck@apple.com>
2024-09-29 09:49:50 +0200

    tweak peek config

--- lua/user/plugins/peek.lua
+++ lua/user/plugins/peek.lua
@@ -3,7 +3,7 @@ return {
 	event = { "VeryLazy" },
 	build = "deno task --quiet build:fast",
 	config = function()
-		require("peek").setup({ app = "browser" })
+		require("peek").setup({ app = "browser", theme = "light" })
 		vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
 		vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 	end,
