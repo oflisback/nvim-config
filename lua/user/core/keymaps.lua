a88e7f8cb2640326a1d8df7e1ac2d7970c2b8230
Ola Flisbäck <ola.flisback@gmail.com>
2024-12-26 08:45:33 +0100

    Add treewalker.nvim

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -11,3 +11,11 @@ keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Oil file manager" })
 
 keymap.set("n", "<Tab>", "<cmd>:bnext<CR>", { desc = "Next buffer" })
 keymap.set("n", "<S-Tab>", "<cmd>:bprev<CR>", { desc = "Prev buffer" })
+
+-- Treewalker
+vim.keymap.set({ "n", "v" }, "<C-k>", "<cmd>Treewalker Up<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-j>", "<cmd>Treewalker Down<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-l>", "<cmd>Treewalker Right<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-h>", "<cmd>Treewalker Left<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-j>", "<cmd>Treewalker SwapDown<cr>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-k>", "<cmd>Treewalker SwapUp<cr>", { noremap = true, silent = true })
