b4244e6e0b30e8706b9517bb22f5aed3d832ebad
Ola Flisbäck <ola.flisback@gmail.com>
2025-03-30 12:44:57 +0200

    Use gemini-2.0-flash

--- lua/user/plugins/parrot.lua
+++ lua/user/plugins/parrot.lua
@@ -10,7 +10,8 @@ local function capture_command_output(cmd)
 	end
 end
 
-local openai_api_key = capture_command_output(os.getenv("HOME") .. "/.config/echo-openai-key.sh 2> /dev/null")
+-- local openai_api_key = capture_command_output(os.getenv("HOME") .. "/.config/echo-openai-key.sh 2> /dev/null")
+local gemini_api_key = capture_command_output(os.getenv("HOME") .. "/.config/echo-gemini-key.sh 2> /dev/null")
 
 return {
 	"frankroeder/parrot.nvim",
@@ -19,10 +20,10 @@ return {
 		require("parrot").setup({
 			-- Providers must be explicitly added to make them available.
 			providers = {
-				openai = {
-					api_key = openai_api_key,
+				gemini = {
+					api_key = gemini_api_key,
 					topic = {
-						model = "o3-mini",
+						model = "gemini-2.0-flash",
 						system_prompt = "You are a general AI assistant.\n\n"
 							.. "The user provided the additional info about how they would like you to respond:\n\n"
 							.. "- If you're unsure don't guess and say you don't know instead.\n"
@@ -33,6 +34,20 @@ return {
 							.. "- Take a deep breath; You've got this!\n",
 					},
 				},
+				-- openai = {
+				-- 	api_key = openai_api_key,
+				-- 	topic = {
+				-- 		model = "o3-mini",
+				-- 		system_prompt = "You are a general AI assistant.\n\n"
+				-- 			.. "The user provided the additional info about how they would like you to respond:\n\n"
+				-- 			.. "- If you're unsure don't guess and say you don't know instead.\n"
+				-- 			.. "- Ask question if you need clarification to provide better answer.\n"
+				-- 			.. "- Think deeply and carefully from first principles step by step.\n"
+				-- 			.. "- If the answer does not need to be lengthy, make it a priority to keep it short.\n"
+				-- 			.. "- Don't elide any code from your output if the answer requires coding.\n"
+				-- 			.. "- Take a deep breath; You've got this!\n",
+				-- 	},
+				-- },
 			},
 		})
 	end,
