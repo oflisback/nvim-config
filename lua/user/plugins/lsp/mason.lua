5cfab583cf029e5826f656b98dff50ef47d82266
Ola Flisbäck <oflisbaeck@apple.com>
2025-06-19 13:56:47 +0200

    Add tailwind lsp

--- lua/user/plugins/lsp/mason.lua
+++ lua/user/plugins/lsp/mason.lua
@@ -31,26 +31,22 @@ return {
 				"ts_ls",
 				"html",
 				"cssls",
+				"tailwindcss", -- Add TailwindCSS LSP
 				"lua_ls",
 				"emmet_ls",
 				"pyright",
 				"gopls",
 			},
-			automatic_installation = true, -- install all listed servers if not already installed
+			automatic_installation = true,
 			handlers = {
-				-- This is the default handler that will be called for all servers
-				-- that don't have a specific handler defined below (e.g., html, cssls, lua_ls, pyright, gopls).
-				-- It will simply set up the LSP client with default capabilities for completion.
+				-- Default handler
 				function(server_name)
 					require("lspconfig")[server_name].setup({
 						capabilities = require("cmp_nvim_lsp").default_capabilities(),
-						-- Add common settings for all LSPs here if needed
-						-- Example: A shared on_attach function (already in your lspconfig.lua)
-						-- on_attach = require("path.to.your_on_attach_function"),
 					})
 				end,
 
-				-- Custom handler for 'ts_ls' (TypeScript Language Server / tsserver)
+				-- Custom handler for 'ts_ls'
 				ts_ls = function()
 					local lspconfig = require("lspconfig")
 					local util = require("lspconfig.util")
@@ -58,16 +54,61 @@ return {
 
 					lspconfig.ts_ls.setup({
 						capabilities = capabilities,
-						-- Activate ts_ls for Node.js/TypeScript projects
-						root_dir = util.root_pattern("package.json", "tsconfig.json", ".git"), -- .git is a common fallback
-						single_file_support = false, -- Prevent it from attaching to single files
+						root_dir = util.root_pattern("package.json", "tsconfig.json", ".git"),
+						single_file_support = false,
 						settings = {
-							-- Potentially useful settings for ts_ls in Node.js contexts
 							typescript = {
 								disableAutomaticTypeAcquisition = true,
 							},
 							javascript = {
 								disableAutomaticTypeAcquisition = true,
+							},
+						},
+					})
+				end,
+
+				tailwindcss = function()
+					local lspconfig = require("lspconfig")
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+					lspconfig.tailwindcss.setup({
+						capabilities = capabilities,
+						filetypes = {
+							"html",
+							"css",
+							"scss",
+							"javascript",
+							"javascriptreact",
+							"typescript",
+							"typescriptreact",
+							"vue",
+							"svelte",
+							"astro",
+							"markdown",
+							"mdx",
+						},
+						settings = {
+							tailwindCSS = {
+								experimental = {
+									classRegex = {
+										-- Add custom regex patterns if you use class names in unusual places
+										"tw`([^`]*)",
+										'tw="([^"]*)',
+										'tw={"([^"}]*)',
+										"tw\\.\\w+`([^`]*)",
+										"tw\\(.*?\\)`([^`]*)",
+									},
+								},
+								validate = true,
+								lint = {
+									cssConflict = "warning",
+									invalidApply = "error",
+									invalidConfigPath = "error",
+									invalidScreen = "error",
+									invalidTailwindDirective = "error",
+									invalidVariant = "error",
+									recommendedVariantOrder = "warning",
+								},
 							},
 						},
 					})
@@ -78,10 +119,10 @@ return {
 		mason_tool_installer.setup({
 			ensure_installed = {
 				"biome",
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
 				"pylint",
 				"eslint",
 			},
